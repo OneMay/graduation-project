2f0298d954575f4b143b36082a4b350e01e91541
OneMay <1102304904@qq.com>
2019-03-13 22:02:10 +0800

    解决打包后样式错乱问题

--- app/mirror/src/main.js
+++ app/mirror/src/main.js
@@ -1,8 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 import axios from 'axios'
@@ -11,6 +9,8 @@ import {store} from './store/store.js'
 import vuescroll from 'vuescroll';
 import ECharts from 'vue-echarts'
 import 'echarts'
+import App from './App'
+import router from './router'
 window.postMirrorData.entities.system='mirror';
 let userInfo=commenMotheds.getCookie('userInfo')
 let username = userInfo?commenMotheds.parserDataToJson(userInfo).mobile:'';
